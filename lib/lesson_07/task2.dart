cf9b75fc4e4fa91c823674845a91d071a1ae5ef2
Roman <vrbox@i.ua>
2026-03-24 17:19:32 +0200

    feat: working with sets

---
+++ lib/lesson_07/task2.dart
@@ -0,0 +1,15 @@
+
+import 'package:dart_app/lesson_07/names_deepseek.dart';
+import 'package:dart_app/lesson_07/names_gpt.dart';
+
+void main() {
+  final intersectionSet = ukrainianNamesDeepseek.intersection(ukrainianNamesGPT);
+  print('Спільні імена:\n$intersectionSet');
+
+  final uniqueToDeepseek = ukrainianNamesDeepseek.difference(ukrainianNamesGPT);
+  print('\nІмена, які є тільки в Deepseek:\n$uniqueToDeepseek');
+
+  final uniqueToGPT = ukrainianNamesGPT.difference(ukrainianNamesDeepseek);
+  print('\nІмена, які є тільки в GPT:\n$uniqueToGPT');
+
+}
